766a8a601091511cb652824283dee1c142c52faf
hannahxu2 <78995850+hannahxu2@users.noreply.github.com>
2021-02-13 16:44:07 +0800

    Update README.py

--- README.py
+++ README.py
@@ -1,6 +1,7 @@
 # Coursera_Capstone.-
-The notebook is mainly used for the Capstone Project
+#The notebook is mainly used for the Capstone Project
 
 import pandas as pd
 import numpy as np
 print('Hello Capstone Project Course!')
+
